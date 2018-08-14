33eb23e17f2f6352592bf297b20399cdd6357efc
zhifeng.wang@e-melt.com <zhifeng.wang@e-melt.com>
2018-08-14 10:24:13 +0800

    go

---
+++ Protectice/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Protectice.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Protectice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Protectice.WebApiApplication" Language="C#" %>
